37dda79043c320a5e6ba8fca8ab2b3e2412dfb66
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2017-05-18 00:44:23 +0200

    Track User updatedAt and createAt.

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -5,20 +5,22 @@ var Types = keystone.Field.Types;
  * User Model
  * ==========
  */
-var User = new keystone.List('User');
+var User = new keystone.List('User', {
+  track: { createdAt: true, updatedAt: true },
+});
 
 User.add({
-	name: { type: Types.Name, required: true, index: true },
-	email: { type: Types.Email, initial: true, required: true, unique: true, index: true },
-	password: { type: Types.Password, initial: true, required: true },
+  name: { type: Types.Name, required: true, index: true },
+  email: { type: Types.Email, initial: true, required: true, unique: true, index: true },
+  password: { type: Types.Password, initial: true, required: true },
     birthdate: { type: Types.Date, required: true, initial: true }
 }, 'Permissions', {
-	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
+  isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
 });
 
 // Provide access to Keystone
 User.schema.virtual('canAccessKeystone').get(function () {
-	return this.isAdmin;
+  return this.isAdmin;
 });
 
 
