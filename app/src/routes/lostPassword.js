f542aaeffba1817f360eb4feaffa98b420f26f8a
Colin Maudry <colin@maudry.com>
2017-06-04 19:42:32 +0200

    Début d'implémentation de mailgun, à tester #7

--- app/src/routes/lostPassword.js
+++ app/src/routes/lostPassword.js
@@ -1,5 +1,6 @@
 import keystone from 'keystone';
 import srs from 'secure-random-string';
+import Mailgun from 'mailgun-js';
 
 const User = keystone.list('User');
 const PasswordResetToken = keystone.list('PasswordResetToken');
@@ -11,8 +12,32 @@ export default async (req, res) => {
 
   if (!!user) {
     const token = PasswordResetToken.model({user: user, token: srs(32)});
+    const api_key = process.env.MAILGUN_API_KEY;
+    const domain = process.env.MAILGUN_DOMAIN;
 
-    // FIXME: send the link with token by email
+    var mailgun = new Mailgun({apiKey: api_key, domain: domain});
+    var data = {
+      from: "no-reply@auth.mavoix.info",
+      to: email,
+      subject: "Réinitialiser votre mot de passe #MAVOIX",
+      html: "Ajouter le HTML du mail ici."
+    };
+
+    //Invokes the method to send emails given the above data with the helper library
+    mailgun.messages().send(data, function (err, body) {
+        //If there is an error, render the error page
+        if (err) {
+            res.render('error', { error : err});
+            console.log("got an error: ", err);
+        }
+        //Else we can greet    and leave
+        else {
+            //Here "submitted.jade" is the view file for this landing page
+            //We pass the variable "email" from the url parameter in an object rendered by Jade
+            //res.render('submitted', { email : req.params.mail });
+            console.log(body);
+        }
+    });
     console.log(token);
 
     await token.save();
